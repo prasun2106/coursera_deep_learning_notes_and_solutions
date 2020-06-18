b370cd8dbd3dc8925e6c50ca85e6685996e44542
Prasun Kumar <prasun_kumar@merilytics.com>
2020-06-18 10:37:03 +0530

    Convolution Forward Added

--- 4.convolutional_network/Convolution_model_Step_by_Step_v2a.py
+++ 4.convolutional_network/Convolution_model_Step_by_Step_v2a.py
@@ -113,7 +113,7 @@ np.random.seed(1)
 # a = np.pad(a, ((0,0), (1,1), (0,0), (3,3), (0,0)), mode='constant', constant_values = (0,0))
 # ```
 
-# In[2]:
+# In[4]:
 
 
 # GRADED FUNCTION: zero_pad
@@ -138,7 +138,7 @@ def zero_pad(X, pad):
     return X_pad
 
 
-# In[3]:
+# In[5]:
 
 
 np.random.seed(1)
@@ -218,7 +218,7 @@ def conv_single_step(a_slice_prev, W, b):
 
     ### START CODE HERE ### (≈ 2 lines of code)
     # Element-wise product between a_slice_prev and W. Do not add the bias yet.
-    s = a_slice_prev*W
+    s = a_slice_prev * W
     # Sum over all entries of the volume s.
     Z = np.sum(s)
     # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
@@ -228,7 +228,7 @@ def conv_single_step(a_slice_prev, W, b):
     return Z
 
 
-# In[8]:
+# In[7]:
 
 
 np.random.seed(1)
@@ -306,7 +306,7 @@ print("Z =", Z)
 # * Remember that `a_prev_pad` is a subset of `A_prev_pad`.  
 #   Think about which one should be used within the for loops.
 
-# In[ ]:
+# In[16]:
 
 
 # GRADED FUNCTION: conv_forward
@@ -340,36 +340,36 @@ def conv_forward(A_prev, W, b, hparameters):
     
     # Compute the dimensions of the CONV output volume using the formula given above. 
     # Hint: use int() to apply the 'floor' operation. (≈2 lines)
-    n_H = (((n_H_prev + 2 * pad - f)/stride) + 1)  #n+2p-f/s + 1
-    n_W = (((n_W_prev + 2 * pad - f)/stride) + 1)
+    n_H = int(((n_H_prev + 2 * pad - f)/stride) + 1)  #n+2p-f/s + 1
+    n_W = int(((n_W_prev + 2 * pad - f)/stride) + 1)  
     
     # Initialize the output volume Z with zeros. (≈1 line)
-    Z = np.zeros((n_H, n_W, n_C))
+    Z = np.zeros((m, n_H, n_W, n_C))
     
     # Create A_prev_pad by padding A_prev
-    A_prev_pad = 
+    A_prev_pad = np.pad(A_prev, ((0,0),(pad, pad), (pad, pad), (0,0)),mode = 'constant')
     
-    for i in range(None):               # loop over the batch of training examples
-        a_prev_pad = None               # Select ith training example's padded activation
-        for h in range(None):           # loop over vertical axis of the output volume
+    for i in range(m):               # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]                #Select ith training example's padded activation
+        for h in range(n_H):            # loop over vertical axis of the output volume
             # Find the vertical start and end of the current "slice" (≈2 lines)
-            vert_start = None
-            vert_end = None
+            vert_start = h*stride
+            vert_end = h*stride + f
             
-            for w in range(None):       # loop over horizontal axis of the output volume
+            for w in range(n_W):       # loop over horizontal axis of the output volume
                 # Find the horizontal start and end of the current "slice" (≈2 lines)
-                horiz_start = None
-                horiz_end = None
+                horiz_start = w*stride
+                horiz_end = w*stride + f
                 
-                for c in range(None):   # loop over channels (= #filters) of the output volume
+                for c in range(n_C):   # loop over channels (= #filters) of the output volume
                                         
                     # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
-                    a_slice_prev = None
+                    a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start: horiz_end, :] #m,n_H_prev,n_W_prev, n_C_prev
                     
                     # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈3 line)
-                    weights = None
-                    biases = None
-                    Z[i, h, w, c] = None
+                    weights = W[:,:,:,c]
+                    biases = b[:,:,:,c]
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, weights, biases)
                                         
     ### END CODE HERE ###
     
@@ -382,7 +382,7 @@ def conv_forward(A_prev, W, b, hparameters):
     return Z, cache
 
 
-# In[ ]:
+# In[17]:
 
 
 np.random.seed(1)
